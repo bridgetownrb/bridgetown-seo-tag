1bd092d3587923f52f56634fcb89463093e2520d
Jared White <jared@jaredwhite.com>
2021-04-17 10:39:34 -0700

    make cops happy

--- lib/bridgetown-seo-tag/builder.rb
+++ lib/bridgetown-seo-tag/builder.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 module Bridgetown
   class SeoTag
     class Builder < Bridgetown::Builder
       def build
         helper "seo", helpers_scope: true do |title: true|
           context = Liquid::Context.new({}, {}, { site: site, page: view.page })
-          tag_output = Liquid::Template.parse("{% seo #{"title=false" unless title} %}").render!(context, {})
+          tag_output = Liquid::Template.parse(
+            "{% seo #{"title=false" unless title} %}"
+          ).render!(context, {})
           tag_output.respond_to?(:html_safe) ? tag_output.html_safe : tag_output
         end
       end
